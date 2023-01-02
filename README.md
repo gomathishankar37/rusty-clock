43e5a837588086d72e144961699706073712952f
gomathishankar37 <gomathishankar37@gmail.com>
2023-01-03 00:09:15 +0530

    fix: add clock feature
    Signed-off-by: gomathishankar37 <gomathishankar37@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# A Rust based Clock
+- Uses chrono Crate to get the Local time
+- Uses Unicode Characters for Clock faces
+- Updates the Clock time every second
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+// use Chorno Date Time Crate
+use chrono::{Local};
+
+// https://www.w3.org/TR/xml-entity-names/025.html
+// A 2D Constant Array with 7 Rows and 11 Columns
+const DIGITS : [[&str; 11]; 7] = [
+    ["┏━┓ ","  ╻  "," ┏━┓ ", " ┏━┓ "," ╻ ╻ "," ┏━┓ "," ┏   "," ┏━┓ "," ┏━┓ "," ┏━┓ ","   "],
+    ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ "," ╻ "],
+    ["┃ ┃ ","  ┃  ","   ┃ ", "   ┃ "," ┃ ┃ "," ┃   "," ┃   ","   ┃ "," ┃ ┃ "," ┃ ┃ ","   "],
+    ["┃ ┃ ","  ┃  "," ┏━┛ ", " ┣━┫ "," ┗━┫ "," ┗━┓ "," ┣━┓ ","   ┃ "," ┣━┫ "," ┗━┫ ","   "],
+    ["┃ ┃ ","  ┃  "," ┃   ", "   ┃ ","   ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ┃ ┃ ","   ┃ ","   "],
+    ["┃ ┃ ","  ┃  "," ┃   ", "   ┃ ","   ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ┃ ┃ ","   ┃ "," ╹ "],
+    ["┗━┛ ","  ╹  "," ┗━━ ", " ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   "],
+];
+
+fn main() {
+    let t = Local::now();
+    loop {
+        let t = Local::now();
+        let time = t.format("%H:%M:%S").to_string();
+        for row in &DIGITS {
+            for character in time.chars() {
+                let col = match character {
+                    '0'..='9' => character as usize - '0' as usize,
+                    ':' => 10,
+                    _ => 10,
+                };
+                print!("{} ", row[col]);
+            }
+            println!();
+        }
+        std::thread::sleep(std::time::Duration::from_millis(999));
+        print!("\x1b[7A");
+    }
+    
+
+}
